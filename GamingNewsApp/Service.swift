4b7ee900f8ae16dc467db724de6d6e5d7c3e8e0b
maartenvanmeersche <maarten.vanmeersche.v4698@student.hogent.be>
2017-01-19 14:51:31 +0100

    first commit

---
+++ GamingNewsApp/Service.swift
@@ -0,0 +1,9 @@
+//
+//  Service.swift
+//  GamingNewsApp
+//
+//  Created by Maarten Van Meersche on 5/01/17.
+//  Copyright © 2017 Maarten Van Meersche. All rights reserved.
+//
+
+import Foundation
---
+++ GamingNewsApp/SettingsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsViewController.swift
+//  GamingNewsApp
+//
+//  Created by Maarten Van Meersche on 19/01/17.
+//  Copyright © 2017 Maarten Van Meersche. All rights reserved.
+//
+
+import Foundation
